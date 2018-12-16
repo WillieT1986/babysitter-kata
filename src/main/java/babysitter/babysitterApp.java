b70762116a41e5a6cbea0791f931509e65b3440d
WillieT1986 <warmad2@hotmail.com>
2018-12-15 22:32:38 -0500

    Refactored babysitterApp -> BabysitterApp. Instantiated Babysitter through babysitterTest and passed 1st Test.

--- src/main/java/babysitter/babysitterApp.java
+++ src/main/java/babysitter/babysitterApp.java
@@ -1,6 +1,6 @@
 package babysitter;
 
-public class babysitterApp {
+public class BabysitterApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- src/test/java/babysitter/babysitterTest.java
+++ src/test/java/babysitter/babysitterTest.java
@@ -1,5 +1,15 @@
 package babysitter;
 
-public class babysitterTest {
+import static org.junit.Assert.assertNotNull;
 
+import org.junit.Test;
+
+public class babysitterTest {
+	
+	@Test
+	public void shouldInitiateBabysitterClass() {
+		Babysitter underTest = new Babysitter();
+		assertNotNull(underTest);
+	}
+	
 }

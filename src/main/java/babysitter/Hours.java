1162f80cc75aab84cb5c41d84875f6f24c4cac0a
WillieT1986 <warmad2@hotmail.com>
2018-12-17 11:12:28 -0500

    Added test for NonWorking hours. Expected 1200 but was 0.

--- src/main/java/babysitter/Hours.java
+++ src/main/java/babysitter/Hours.java
@@ -1,15 +1,43 @@
 package babysitter;
 
 public class Hours {
+	
 	private int work;
+	private int nonWork;
 	
-	
-	public Hours(int work) {
+	public Hours(int work, int nonWork) {
 		this.work = work;
+		this.nonWork = nonWork;
 	}
 
-	public int getWork() {
-		return 1100;
+	public int getWork(int workHour) {
+			return work;
+		}
+
+	public int getNonWork() {
+		return 0;
 	}
+	
+	
+	private int workHour(int workHour) {
+		if (workHour == 1700 && workHour <= 1759);
+		else if (workHour == 1800 && workHour <= 1859);
+		else if (workHour == 1900 && workHour <= 1959);
+		else if (workHour == 2000 && workHour <= 2059);
+		else if (workHour == 2100 && workHour <= 2159);
+		else if (workHour == 2200 && workHour <= 2259);
+		else if (workHour == 2300 && workHour <= 2359);
+//		else if (workHour == 0000 && workHour <= 0059L);
+//		else if (workHour == 0100 && workHour <= 0159L);
+//		else if (workHour == 0200 && workHour <= 0259L);
+//		else if (workHour == 0300 && workHour <= 0359L);
+		else if (workHour == 0400);
+		
+		return workHour;
+	}
+
+
+	
+	
 
 }
--- src/test/java/babysitter/HoursTest.java
+++ src/test/java/babysitter/HoursTest.java
@@ -9,15 +9,22 @@ public class HoursTest {
 	
 	@Test
 	public void shouldConstructWorkingHours() {
-		Hours underTest = new Hours(1100);
+		Hours underTest = new Hours(1700, 1200);
 		assertNotNull(underTest);
 	}
 	
 	@Test
 	public void shouldReturnWorkingHours() {
-		Hours underTest = new Hours(1100);
-		int check = underTest.getWork();
-		assertEquals(1100, check);
+		Hours underTest = new Hours(1700, 1200);
+		int check = underTest.getWork(1700);
+		assertEquals(1700, check);
+	}
+	
+	@Test
+	public void shouldReturnNonWorkingHour() {
+		Hours underTest = new Hours(1100, 1200);
+		int check = underTest.getNonWork();
+		assertEquals(1200, check);
 	}
 	
 }

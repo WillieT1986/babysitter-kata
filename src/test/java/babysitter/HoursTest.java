e69155ab0c291dfb8b2e1894d2c5cadbfbb81389
WillieT1986 <warmad2@hotmail.com>
2018-12-17 11:54:41 -0500

    Did some refactoring.

--- src/test/java/babysitter/HoursTest.java
+++ src/test/java/babysitter/HoursTest.java
@@ -3,27 +3,34 @@ package babysitter;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class HoursTest {
-	
+	private static final int WORK = 1700;
+	private static final int NONWORK = 1200;
+
+	private Hours underTest;
+
+	@Before
+	public void setUp() {
+		underTest = new Hours(WORK, NONWORK);
+	}
+
 	@Test
 	public void shouldConstructWorkingHours() {
-		Hours underTest = new Hours(1700, 1200);
 		assertNotNull(underTest);
 	}
-	
+
 	@Test
 	public void shouldReturnWorkingHours() {
-		Hours underTest = new Hours(1700, 1200);
 		int check = underTest.getWork(1800);
-		assertEquals(1700, check);
+		assertEquals(WORK, check);
 	}
-	
+
 	@Test
 	public void shouldReturnNonWorkingHour() {
-		Hours underTest = new Hours(1700, 1200);
 		int check = underTest.getNonWork(1100);
-		assertEquals(1200, check);
+		assertEquals(NONWORK, check);
 	}
 }

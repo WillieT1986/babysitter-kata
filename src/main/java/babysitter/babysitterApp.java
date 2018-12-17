d4ed72be186895d6bc5687eca63ab7987e8fb1cf
WillieT1986 <warmad2@hotmail.com>
2018-12-17 07:51:02 -0500

    Minor changes to clean up and organize on Console for better readability.

--- src/main/java/babysitter/babysitterApp.java
+++ src/main/java/babysitter/babysitterApp.java
@@ -4,15 +4,15 @@ public class BabysitterApp {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Intro to this program goes here.");
+		System.out.println("Intro to this program goes here.\n");
 		
-		System.out.println("Working Hours: 1700hrs to 0400hrs");
-		System.out.println("NonWorking Hours: 0401hours to 1659hrs");
+		System.out.println("Working Hours:     1700hrs to 0400hrs");
+		System.out.println("Non-Working Hours: 0401hrs to 1659hrs\n");
 		
 		System.out.println("Pay Rates Per Family");
 		System.out.println("Family A: $15.00 - 1700hrs to 2300hrs | $20.00 - 2300hrs to 0400hrs");
-		System.out.println("Family B: $12.00 - 1700hrs to 2200hrs | $8.00 - 2200hrs to 0000hrs | $16.00 - 0000hrs to 0400hrs");
-		System.out.println("Family C: $21.00 - 1700hrs to 2100hrs | $15.00 - 2100hrs to 0400hrs");
+		System.out.println("Family B: $12.00 - 1700hrs to 2200hrs | $8.00  - 2200hrs to 0000hrs | $16.00 - 0000hrs to 0400hrs");
+		System.out.println("Family C: $21.00 - 1700hrs to 2100hrs | $15.00 - 2100hrs to 0400hrs\n");
 		
 		System.out.println("");
 		

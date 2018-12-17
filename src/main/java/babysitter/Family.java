7637e3c9fd8d1e930dd10d2aef84ef11fccfe501
WillieT1986 <warmad2@hotmail.com>
2018-12-17 12:31:59 -0500

    Created Test to retrieve a Family Name(Letter). Test expected name but was null.

--- src/main/java/babysitter/Family.java
+++ src/main/java/babysitter/Family.java
@@ -1,5 +1,14 @@
 package babysitter;
 
 public class Family {
+	private String familyname;
+
+	public Family(String familyName) {
+		this.familyname = familyName;
+	}
+
+	public String getFamilyName() {
+		return null;
+	}
 
 }
--- src/test/java/babysitter/familyTest.java
+++ src/test/java/babysitter/familyTest.java
@@ -1,15 +1,23 @@
 package babysitter;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 
 public class FamilyTest {
 
-	
 	@Test
 	public void shouldInstantiateFamily() {
-		Family underTest = new Family();
+		Family underTest = new Family("name");
 		assertNotNull(underTest);
 	}
+
+	@Test
+	public void shouldRetrieveFamilyLetter() {
+		Family underTest = new Family("name");
+		String check = underTest.getFamilyName();
+		assertEquals("name", check);
+	}
+
 }

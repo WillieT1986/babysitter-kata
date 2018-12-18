e9ab075762dec4c3d3e93926ee0d098f02abaf6e
WillieT1986 <warmad2@hotmail.com>
2018-12-18 16:08:10 -0500

    Formatting Layout.

--- src/main/java/babysitter/babysitterApp.java
+++ src/main/java/babysitter/babysitterApp.java
@@ -1,18 +1,21 @@
 package babysitter;
 
+import java.util.Scanner;
+
 public class BabysitterApp {
 
 	public static void main(String[] args) {
-		// Scanner input = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 
-		System.out.println("Intro to this program goes here.\n");
+		System.out.println("This is my version of the Babysitter Kata.");
+		System.out.println("\tThank You for your time.\n");
 
 		System.out.println("This is a 24 hour clock time. 1300 to 0000 is 1pm to 12am.");
 		System.out.println("To make it easier, if you subtract 12 from 1300 you get 1pm.\n");
 		System.out.println("Working Hours:     1700hrs to 0400hrs");
-		System.out.println("Non-Working Hours: 0401hrs to 1659hrs\n");
+		System.out.println("Non-Working Hours: 0401hrs to 1659hrs\n\n");
 
-		System.out.println("Pay Rates Per Family");
+		System.out.println("\t\t\tPay Rates Per Family");
 		System.out.println("Family A: $15.00 - 1700hrs to 2300hrs | $20.00 - 2300hrs to 0400hrs");
 		System.out.println(
 				"Family B: $12.00 - 1700hrs to 2200hrs | $8.00  - 2200hrs to 0000hrs | $16.00 - 0000hrs to 0400hrs");

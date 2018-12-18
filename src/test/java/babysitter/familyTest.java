f3ecf2446257921ad540b93f3203894221e8dd70
WillieT1986 <warmad2@hotmail.com>
2018-12-17 20:35:29 -0500

    Refactored. Tests still pass.

--- src/test/java/babysitter/familyTest.java
+++ src/test/java/babysitter/familyTest.java
@@ -3,33 +3,40 @@ package babysitter;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class FamilyTest {
+	private static final String FAMILY_LETTER = "name";
+	private static final int PAY_RATE = 15;
+	private static final int HOURS = 1700;
+
+	private Family underTest;
+
+	@Before
+	public void setUp() {
+		underTest = new Family(FAMILY_LETTER, PAY_RATE, HOURS);
+	}
 
 	@Test
 	public void shouldInstantiateFamily() {
-		Family underTest = new Family("name", 0, 0);
 		assertNotNull(underTest);
 	}
 
 	@Test
 	public void shouldRetrieveFamilyLetter() {
-		Family underTest = new Family("name", 0, 0);
 		String check = underTest.getFamilyName("name");
 		assertEquals("name", check);
 	}
 
 	@Test
 	public void shouldReturnFamilyPayRate() {
-		Family underTest = new Family("name", 15, 0);
 		int check = underTest.getPayRate();
 		assertEquals(15, check);
 	}
 
 	@Test
 	public void shouldReturnFamilyHours() {
-		Family underTest = new Family("name", 0, 1700);
 		int check = underTest.getFamilyHours();
 		assertEquals(1700, check);
 	}

18ead7fcaf900293397ea379fcb52ddb6abcebd9
WillieT1986 <warmad2@hotmail.com>
2018-12-20 20:10:24 -0500

    Got program to run through these options: Y, A, 1700, 1701 successfully as is.

--- src/main/java/babysitter/babysitterApp.java
+++ src/main/java/babysitter/babysitterApp.java
@@ -7,11 +7,11 @@ public class BabysitterApp {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 
-		String decision = input.nextLine();
+		String decision = ("");
 		while (!decision.equalsIgnoreCase("Yes")) {
 
-			System.out.println("\t\tThis is my version of the Babysitter Kata.");
-			System.out.println("\t\t\tThank You for your time.\n");
+			System.out.println("\t\t\tBabysitter Kata.");
+			System.out.println("\t\tThank You for your time.\n");
 
 			System.out.println("This is a 24 hour clock time. 1300 to 0000 is 1pm to 12am.");
 			System.out.println("To make it easier, if you subtract 12 from 1300 you get 1pm.\n");
@@ -46,26 +46,16 @@ public class BabysitterApp {
 
 				System.out.println("\nEnter End Time: ");
 				int endTime = input.nextInt();
-				if (endTime >= 1700 && endTime <= 2359 && endTime >= 0000 && endTime <= 0400) {
+				if (!(endTime >= 1700 && endTime <= 2359 && endTime >= 0000 && endTime <= 0400)) {
 				} else {
 					System.out.println("Please Enter a Working Hour and not a Non-Working Hour.\n");
 				}
 
 				System.out.println(
 						"\nYou work for a total of " + "hour's tonight and made a total of $" + " tonight.\n\n");
-				System.out.println("Would you like to Continue?");
-				System.out.println("Y or N");
 
 			} else if (!dayDecision.equalsIgnoreCase("Y")) {
-				System.out.println("Maybe Next Time...\n");
-			} else {
-				System.out.println("Would you like to exit this program?");
-				System.out.println("Enter Yes or No");
-				if (decision.equalsIgnoreCase("Yes\n")) {
-					System.out.println("Program Terminated...\n");
-				} else {
-					System.out.println("What would you like to do now?\n");
-				}
+				System.out.println("Maybe Next Time...\n\n\n\n");
 			}
 		}
 		input.close();
